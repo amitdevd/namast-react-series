67863b72a952f7e29eac5e05b5bd6a3c01c0755c
amitdwivedi12 <dwivedi.amit154@gmail.com>
2025-12-06 18:51:43 +0530

    ui add

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,34 +1,43 @@
-import RestaurantCard from "./RestaurantCard";
-import swigywork from "../data/Swigywork.json";
-import { useState } from "react";
+import RestaurantCard from "./RestaurantCard"; 
+import swigywork from "../data/Swigywork.json"; 
+import { useState } from "react"; 
+import Shimmer from "./Shimmer"; 
 
-const Body = () => {
-
-    const[listOfRestaurants, setListOfRestaurants] = useState(swigywork);
-
-    return (
-        <div className="body">
-            <div className="search">
-                <input type="text" className="form-control" placeholder="Search..." />
-                <button className="filter-btn" 
-                onClick={() => {
-                    const filteredList = listOfRestaurants.filter(
-                        (res) => res.avgRating > 4 
-                    );
-                    setListOfRestaurants(filteredList);
-                }} 
-                >Top Rated Restaurants</button>
-            </div>
-            <div className="res-container">
-                {
-                    listOfRestaurants.map((item, index) => (
-                        <RestaurantCard key={item.id} swigywork={item} />
-                    ))
-                }
-               {/* <RestaurantCard swigywork={swigywork[0]} /> */}
-            </div>
-        </div>
-    )
-}
-
+const Body = () => { 
+    // Load restaurants from local JSON file 
+    
+    const [listOfRestaurants, setListOfRestaurants] = useState(swigywork); 
+    
+    // Search input text 
+    const [searchText, setSearchText] = useState(""); 
+    
+    return listOfRestaurants.length === 0 ? ( 
+        <Shimmer /> ) : ( 
+        <div className="body"> 
+            <div className="search"> 
+                <div> 
+                    <input type="text" className="form-control" value={searchText} placeholder="Search..." onChange={(e) => setSearchText(e.target.value)} /> 
+                    {/* Search Button */} 
+                    <button onClick={() => { 
+                        const filteredRestaurant = swigywork.filter((res) => 
+                            res.name .toLowerCase() .includes(searchText.toLowerCase()) ); 
+                        setListOfRestaurants(filteredRestaurant); }} 
+                        >Search</button> 
+                </div> 
+                        
+                {/* Top Rated Filter */} 
+                <button className="filter-btn" onClick={() => { 
+                    const filteredList = swigywork.filter( (res) => 
+                        res.avgRating > 4 ); setListOfRestaurants(filteredList); }} 
+                    > Top Rated Restaurants </button> 
+            </div> 
+            {/* Restaurant List */} 
+            <div className="res-container"> 
+                {listOfRestaurants.map((item) => ( 
+                    <RestaurantCard key={item.id} swigywork={item} /> ))} 
+            </div> 
+        </div> 
+    ); 
+}; 
+        
 export default Body;
